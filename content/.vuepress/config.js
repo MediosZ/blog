7a81439aaa59e67fe72788e0e14be2637252374e
Tricster <mediosrity@gmail.com>
2022-11-17 10:31:01 +0800

    change base url
    Signed-off-by: Tricster <mediosrity@gmail.com>

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -1,95 +1,99 @@
 const feed_options = {
-  canonical_base: 'https://blog.mediosz.club/feed',
+  canonical_base: "https://blog.mediosz.club/feed",
 };
 module.exports = {
-    title: 'Avalon',
-    theme: '@vuepress/theme-blog',
-    description: 'Just a place called Avalon',
-    base: "/blog/",
-    dest: "public",
-    plugins: [
-        '@vuepress/back-to-top',
-        '@vuepress/nprogress',
-        ['@vuepress/medium-zoom', {
-            selector: '.vuepress-blog-theme-content :not(a) > img',
-            options: {
-              margin: 16
-            }
-        }],
-        ['reading-progress', {
-            readingDir: "_posts"
-        }]
+  title: "Avalon",
+  theme: "@vuepress/theme-blog",
+  description: "Just a place called Avalon",
+  base: "/",
+  dest: "public",
+  plugins: [
+    "@vuepress/back-to-top",
+    "@vuepress/nprogress",
+    [
+      "@vuepress/medium-zoom",
+      {
+        selector: ".vuepress-blog-theme-content :not(a) > img",
+        options: {
+          margin: 16,
+        },
+      },
     ],
-    themeConfig: {
-      nav: [
+    [
+      "reading-progress",
+      {
+        readingDir: "_posts",
+      },
+    ],
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: "中文",
+        link: "/",
+      },
+      {
+        text: "English",
+        link: "/en/",
+      },
+      {
+        text: "标签",
+        link: "/tag/",
+      },
+    ],
+    directories: [
+      {
+        id: "post",
+        dirname: "_posts",
+        path: "/",
+      },
+      {
+        id: "en",
+        dirname: "_en",
+        path: "/en/",
+        itemPermalink: "/en/:year/:month/:day/:slug",
+        title: "English",
+        layout: "IndexPost",
+        itemLayout: "Post",
+      },
+    ],
+    summaryLength: 150,
+    footer: {
+      contact: [
         {
-          text: '中文',
-          link: '/',
+          type: "gitlab",
+          link: "https://git.mediosz.club/Tric",
         },
         {
-          text: 'English',
-          link: '/en/',
+          type: "github",
+          link: "https://github.com/MediosZ",
         },
         {
-          text: '标签',
-          link: '/tag/',
+          type: "mail",
+          link: "mailto:mediosrity@qq.com",
         },
       ],
-      directories: [
+      copyright: [
         {
-          id: 'post',
-          dirname: '_posts',
-          path: '/',
+          text: "晋ICP备17009000",
+          link: "http://www.beian.miit.gov.cn/",
         },
-        {
-          id: 'en',
-          dirname: '_en',
-          path: '/en/',
-          itemPermalink: '/en/:year/:month/:day/:slug',
-          title: 'English',
-          layout: 'IndexPost',
-          itemLayout: 'Post',
-        }
       ],
-      summaryLength: 150,
-      footer: {
-
-        contact: [ 
-          {
-            type: 'gitlab',
-            link: 'https://git.mediosz.club/Tric',
-          },
-          {
-            type: 'github',
-            link: 'https://github.com/MediosZ',
-          },
-          {
-            type: 'mail',
-            link: 'mailto:mediosrity@qq.com',
-          },
-
-        ],
-        copyright: [
-          {
-            text: '晋ICP备17009000',
-            link: 'http://www.beian.miit.gov.cn/',
-          },
-        ],
-      },
-      feed: {
-        canonical_base: 'https://blog.mediosz.club',
-      },
-      smoothScroll: true,
-      pwa: true,
     },
-    markdown: {
-      extendMarkdown: md => {
-        // use more markdown-it plugins!
-        md.use(require('markdown-it-footnote'))
-      },
-      extractHeaders: [ 'h1', 'h2', 'h3' ],
-      toc: {
-        includeLevel: [1, 2, 3]
-      }
-    }
-  }
+    feed: {
+      canonical_base: "https://blog.mediosz.club",
+    },
+    smoothScroll: true,
+    pwa: true,
+  },
+  markdown: {
+    extendMarkdown: (md) => {
+      // use more markdown-it plugins!
+      md.use(require("markdown-it-footnote"));
+    },
+    extractHeaders: ["h1", "h2", "h3"],
+    toc: {
+      includeLevel: [1, 2, 3],
+    },
+  },
+};
